bab5dc44f7755da09e09c8b1da5d5d92ad6ed558
inodentry <inodentry@users.noreply.github.com>
2023-06-19 10:03:21 +0000

    deploy: bevy-cheatbook/bevy-cheatbook@165b722f7554146e0fe931c4312659861cc54971

--- code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.d
+++ code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.d
@@ -1,6 +1,7 @@
-/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.rmeta: src/lib.rs src/input/char.rs
+/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.rmeta: src/lib.rs src/input/char.rs src/input/mouse.rs
 
-/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.d: src/lib.rs src/input/char.rs
+/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-03bb5867ab57e82c.d: src/lib.rs src/input/char.rs src/input/mouse.rs
 
 src/lib.rs:
 src/input/char.rs:
+src/input/mouse.rs:
