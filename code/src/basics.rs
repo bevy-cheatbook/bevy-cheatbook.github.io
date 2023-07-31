1840ac8ec797b936f453d7cd65ffbc43ae68a02f
inodentry <inodentry@users.noreply.github.com>
2023-07-31 13:21:59 +0000

    deploy: bevy-cheatbook/bevy-cheatbook@995c62ec8cf1ab76b0ccc2ec2cf875aa00731056

--- code/src/basics.rs
+++ code/src/basics.rs
@@ -436,16 +436,6 @@ fn spawn_special_entity(
 }
 // ANCHOR_END: spatialbundle
 
-fn insert_visibilitybundle(
-    mut commands: Commands,
-) {
-    let parent = Entity::from_raw(0);
-// ANCHOR: insert-visibilitybundle
-    commands.entity(parent)
-        .insert(VisibilityBundle::default());
-// ANCHOR_END: insert-visibilitybundle
-}
-
 // ANCHOR: propagation
 fn spawn_toplevel_entity(
     mut commands: Commands,
@@ -2092,6 +2082,5 @@ pub fn _main_all() {
         .add_system(despawn_child)
         .add_system(inflate_balloons)
         .add_system(reveal_map)
-        .add_system(insert_visibilitybundle)
         .run();
 }
--- code011/target/debug/deps/cheatbook011-f89bf742d303472d.d
+++ code011/target/debug/deps/cheatbook011-f89bf742d303472d.d
@@ -1,8 +1,10 @@
-/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-f89bf742d303472d.rmeta: src/lib.rs src/features/log.rs src/input/char.rs src/input/mouse.rs src/programming/app_builder.rs src/programming/events.rs src/programming/intro_code.rs src/programming/res.rs src/programming/systems.rs
+/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-f89bf742d303472d.rmeta: src/lib.rs src/pitfalls/d3_not_rendering.rs src/pitfalls/uv_coordinates.rs src/features/log.rs src/input/char.rs src/input/mouse.rs src/programming/app_builder.rs src/programming/events.rs src/programming/intro_code.rs src/programming/res.rs src/programming/systems.rs
 
-/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-f89bf742d303472d.d: src/lib.rs src/features/log.rs src/input/char.rs src/input/mouse.rs src/programming/app_builder.rs src/programming/events.rs src/programming/intro_code.rs src/programming/res.rs src/programming/systems.rs
+/home/runner/work/bevy-cheatbook/bevy-cheatbook/src/code011/target/debug/deps/cheatbook011-f89bf742d303472d.d: src/lib.rs src/pitfalls/d3_not_rendering.rs src/pitfalls/uv_coordinates.rs src/features/log.rs src/input/char.rs src/input/mouse.rs src/programming/app_builder.rs src/programming/events.rs src/programming/intro_code.rs src/programming/res.rs src/programming/systems.rs
 
 src/lib.rs:
+src/pitfalls/d3_not_rendering.rs:
+src/pitfalls/uv_coordinates.rs:
 src/features/log.rs:
 src/input/char.rs:
 src/input/mouse.rs:
